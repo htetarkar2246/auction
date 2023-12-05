1437408824eb81bb3c08304fe8758295455402d1
Htet ArKar <htetarkarlevi@gmail.com>
2023-12-05 18:01:22 +0630

    final

--- client.py
+++ client.py
@@ -1,95 +1,103 @@
-import socket #import for socket
-import re #import for regular expressions
+import socket  # import for socket
+import re  # import for regular expressions
 from datetime import datetime
+import json
+
+
 class Client:
     def __init__(self, ip, port):
         self.ip = ip
         self.port = port
-        
+
     def connect_to_server(self):
         try:
-            client_socket:socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket: socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             client_socket.connect((self.ip, self.port))
             client_socket.send(bytes("Hello!!", "utf-8"))
             recv = client_socket.recv(1024).decode()
             print(recv)
-            #program started
-            program:Program = Program(client_socket)
+            # program started
+            program: Program = Program(client_socket)
             program.menu()
         except Exception as err:
             print(err)
 
-class User:
-    def __init__(self, username:str, password:str, email:str, phone:str):
-        # User initialization with validation and loading from a file
-        self.username:str = username
-        self.password:str = password
-        self.email:str = self.validate_email(email)
-        self.phone:str = self.validate_phone(phone)
-        self.users:list = []
-   
-    def validate_email(self, email:str):
-            # Validate email format
-            email_pattern = r'^\S+@\S+\.\S+$'
-            if re.match(email_pattern, email):
-                return email
-            else:
-                return -1
 
-    def validate_phone(self, phone:str):
-            # Validate phone number format (numeric characters only)
-            phone_pattern = r'^[0-9]+$'
-            if re.match(phone_pattern, phone):
-                return phone
-            else:
-                return -1
+def validate_email(email: str):
+    # Validate email format
+    email_pattern = r'^\S+@\S+\.\S+$'
+    if re.match(email_pattern, email):
+        return email
+    else:
+        return -1
+
+
+def validate_phone(phone: str):
+    # Validate phone number format (numeric characters only)
+    phone_pattern = r'^[0-9]+$'
+    if re.match(phone_pattern, phone):
+        return phone
+    else:
+        return -1
+
+
+class User:
+    def __init__(self, username: str, password: str, email: str, phone: str):
+        # User initialization with validation and loading from a file
+        self.username: str = username
+        self.password: str = password
+        self.email: str = validate_email(email)
+        self.phone: str = validate_phone(phone)
+        self.users: list = []
+
+
+def validate_date(date: str):
+    # Validate date format
+    date_pattern = re.compile(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$')
+    if date_pattern.match(date):
+        return date
+    else:
+        return None
+
 
 class Auction:
-    def __init__(self,title:str,description:str,end_time:str):
+    def __init__(self, title: str, description: str, end_time: str):
         self.title: str = title
         self.description: str = description
-        self.end_time: str = self.validate_date(end_time)
+        self.end_time: str = validate_date(end_time)
 
-    def validate_date(self, date: str):
-        # Validate date format
-        date_pattern = re.compile(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$')
-        if date_pattern.match(date):
-            return date
-        else:
-            return None
+
 class Program:
-    def __init__(self,client_socket:socket):
+    def __init__(self, client_socket: socket):
         self.client_socket: socket = client_socket
 
     def menu(self):
         # Main menu for user interaction
         try:
-            option:str= input("\n1: Register\n2: Auction\n3: Bid\n4: Exit\n")
-            self.client_socket.send(bytes(option,'utf-8'))
+            option: str = input("\n1: Register\n2: Login\n3: Exit\n")
+            self.client_socket.send(bytes(option, 'utf-8'))
             if option == '1':
                 self.register_user()
             elif option == '2':
-                self.create_auction()
+                self.login()
             elif option == '3':
-                self.place_bid()
-            elif option == '4':
                 print("\nGoodbye!")
                 exit()
             else:
                 print("\nInvalid option. Please try again.")
-        except Exception as err:
-                print(err)
                 self.menu()
+        except Exception as err:
+            print(err)
+            self.menu()
 
     def register_user(self):
         # User registration process
-        username:str = input("\nEnter username: ")
-        password:str = input("Enter password: ")
-        email:str = input("Enter email: ")
-        phone:str = input("Enter phone number: ")
-
-        #validate email and phone number
+        username: str = input("\nEnter username: ")
+        password: str = input("Enter password: ")
+        email: str = input("Enter email: ")
+        phone: str = input("Enter phone number: ")
         user: User = User(username, password, email, phone)
+        # validate email and phone number
         if user.email == -1 and user.phone == -1:
             print("\nInvalid Email & Phone Number Format!!")
             self.menu()
@@ -101,37 +109,135 @@ class Program:
             self.menu()
         else:
             # registered user's data are sent to server
-            self.client_socket.send(bytes(username+'||'+password+'||'+email+'||'+phone,'utf-8'))
+            self.client_socket.send(bytes(username + '||' + password + '||' + email + '||' + phone, 'utf-8'))
             reply = self.client_socket.recv(1024).decode()
             print(reply)
             self.menu()
+
+    def login(self):
+        try:
+            option: str = input("\n1:Login with Email\n2:Login with Phone\n3:Menu\n")
+            if option == '1':
+                email: str = input("Enter email: ")
+                password: str = input("Enter password: ")
+                email: str = validate_email(email)
+                if email == -1:
+                    print("\nInvalided Email Format!!")
+                    self.login()
+                else:
+                    self.client_socket.send(bytes('1', 'utf-8'))
+                    self.client_socket.send(bytes(email + '||' + password, 'utf-8'))
+                    reply: str = self.client_socket.recv(1024).decode()
+                    print(reply)
+                    if reply == '\nIncorrect Password!!' or reply == '\nEmail Cannot Be Found!!':
+                        self.login()
+                    else:
+                        self.sec_menu()
+            elif option == '2':
+                phone: str = input("Enter Phone: ")
+                password: str = input("Enter password: ")
+                phone: str = validate_phone(phone)
+                if phone == -1:
+                    print("\nInvalided Phone Format!!")
+                    self.login()
+                else:
+                    self.client_socket.send(bytes('2', 'utf-8'))
+                    self.client_socket.send(bytes(phone + '||' + password, 'utf-8'))
+                    reply: str = self.client_socket.recv(1024).decode()
+                    print(reply)
+                    if reply == '\nIncorrect Password!!' or reply == '\nEmail Cannot Be Found!!':
+                        self.login()
+                    else:
+                        self.sec_menu()
+            elif option == '3':
+                self.menu()
+            else:
+                print("\nUnknown Input!!")
+                self.login()
+        except Exception as err:
+            print(err)
+            self.login()
 
     def create_auction(self):
-        title:str = input("Enter auction title: ")
-        description:str = input("Enter auction description: ")
-        end_time:str = input("Enter auction end time (YYYY-MM-DD HH:MM:SS): ")
+        title: str = input("Enter auction title: ")
+        description: str = input("Enter auction description: ")
+        price: int = int(input("Enter Start Price (MMK): "))
+        end_time: str = input("Enter auction end time (YYYY-MM-DD HH:MM:SS): ")
 
-        auction: Auction = Auction(title,description,end_time)
-        current_time: str = datetime.now().strftime('%Y-%m-%d %H:%M:%S')#generating curren_time
+        auction: Auction = Auction(title, description, end_time)
+        current_time: str = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # generating curren_time
         if auction.end_time is None:
             print("\nInvalid Date-Time Format!!")
-            self.menu()
+            self.sec_menu()
         # Comparing Time
-        elif datetime.strptime(auction.end_time,'%Y-%m-%d %H:%M:%S') <= datetime.strptime(current_time,'%Y-%m-%d %H:%M:%S'):
+        elif datetime.strptime(auction.end_time, '%Y-%m-%d %H:%M:%S') <= datetime.strptime(current_time,
+                                                                                           '%Y-%m-%d %H:%M:%S'):
             print("\nInvalid Time!!")
-            self.menu()
+            self.sec_menu()
         else:
-            self.client_socket.send(bytes(title+'||'+description+'||'+end_time,'utf-8'))
+            self.client_socket.send(bytes(title + '||' + description + '||' + end_time + '||' + str(price), 'utf-8'))
             reply = self.client_socket.recv(1024).decode()
             print(reply)
-            self.menu()
+            self.sec_menu()
 
+    def sec_menu(self):
+        try:
+            option: str = input("\n1:Create Auction\n2:Bid\n3:Show Auctions\n4:History Auction\n5:Menu\n")
+            self.client_socket.send(bytes(option, 'utf-8'))
+            if option == '1':
+                self.create_auction()
+            elif option == '2':
+                self.place_bid()
+            elif option == '3':
+                self.show_auctions()
+            elif option == '4':
+                self.history_auction()
+            elif option == '5':
+                self.menu()
+        except Exception as err:
+            print(err)
+            self.sec_menu()
 
     def place_bid(self):
-        # Placeholder for bid placement logic
-        pass
-        
-        
+        try:
+            auction_id: int = int(input("Enter Auction ID to Place Bid: "))
+            bid_amount: int = int(input("Enter Bid Amount: "))
+            self.client_socket.send(bytes(str(auction_id) + '||' + str(bid_amount), 'utf-8'))
+            auction: dict = json.loads(self.client_socket.recv(1024).decode())
+            print("Auction ID: ", auction['id'])
+            print("Title: ", auction['title'])
+            print("Description: ", auction['description'])
+            print("End Time: ", auction['end_time'])
+            print("Price: ", auction['price'])
+            print("Highest Price: ", auction['highest_price'])
+            self.sec_menu()
+        except Exception as err:
+            print(err)
+            self.sec_menu()
+
+    def show_auctions(self):
+        auctions_json: json = self.client_socket.recv(1024).decode()
+        auctions: list = json.loads(auctions_json)
+        # display auctions
+        for i in range(len(auctions)):
+            print('\nAuction ID: '+str(auctions[i]['id']))
+            print('Title: '+auctions[i]['title'])
+            print('Description: '+auctions[i]['description'])
+            print('End Time: '+auctions[i]['end_time'])
+            print('Price:' + str(auctions[i]['price']))
+            print('Highest Price:' + str(auctions[i]['highest price']))
+            print('\n')
+        self.sec_menu()
+
+    def history_auction(self):
+        auction_id: int = int(input("\nEnter Auction ID: "))
+        self.client_socket.send(bytes(str(auction_id), 'utf-8'))
+        recv_bids: list = json.loads(self.client_socket.recv(1024).decode())
+        for bid in recv_bids:
+            print(bid['user'], 'bid at ', bid['bid_time'], ' with amount ', bid['bid_amount'])
+        self.sec_menu()
+
+
 if __name__ == '__main__':
-    client: Client = Client('localhost',8888)
+    client: Client = Client('localhost', 8888)
     client.connect_to_server()
--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 import socket  # import for socket
+import re  # import for regular expressions
+import json
+from datetime import datetime
 
 
 class Server:
@@ -11,6 +14,11 @@ class Server:
         # using Auction class
         # auction db automatically loaded
         self.auction: Auction = Auction()
+        # using Bid class
+        # auction db automatically loaded
+        self.bid: Bid = Bid()
+        # variable for logged user
+        self.log_user_id: int = -1
 
     def start(self):
         server_socket: socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -31,40 +39,132 @@ class Server:
             recv = sock.recv(1024).decode()
             if recv == 'Hello!!':
                 sock.send(bytes("Welcome To Auction!!", "utf-8"))
+                self.menu(sock)
 
-                while True:
-                    option = sock.recv(1024).decode()
-                    if option == '1':
-                        username, password, email, phone = sock.recv(1024).decode().split('||')
-                        # temp_db to store recently registered user's data
+    def menu(self, sock):
+        option = sock.recv(1024).decode()
+        if option == '1':
+            self.register(sock)
+        elif option == '2':
+            self.login(sock)
+
+    def sec_menu(self, sock):
+        option = sock.recv(1024).decode()
+        if option == '1':
+            self.create_auction(sock)
+        elif option == '2':
+            auction_id, bid_amount = sock.recv(1024).decode().split('||')
+            for i in range(len(self.auction.auctions)):
+                if self.auction.auctions[i]['id'] == int(auction_id):
+                    if self.auction.auctions[i]['highest_price'] < int(bid_amount):
+                        self.auction.auctions[i]['highest_price'] = int(bid_amount)
+                        auction: json = json.dumps(self.auction.auctions[i])
+                        sock.send(bytes(auction, 'utf-8'))
+                        self.auction.db_saving()
                         temp_db: dict = {
-                            "username": username,
-                            "password": password,
-                            "email": email,
-                            "phone": phone
+                            'auction_id': self.auction.auctions[i]['id'],
+                            'user': self.user.users[self.log_user_id]['username'],
+                            'bid_amount': self.auction.auctions[i]['highest_price'],
+                            'bid_time': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                         }
-                        self.user.users.append(temp_db)  # appending temp_db to users
-                        self.user.db_saving()  # saving users to database
+                        self.bid.bids.append(temp_db)
+                        self.bid.db_saving()
+                        self.sec_menu(sock)
+        elif option == '3':
+            auctions: json = json.dumps(self.auction.auctions)
+            sock.send(bytes(auctions, 'utf-8'))
+            self.sec_menu(sock)
+        elif option == '4':
+            auction_id = sock.recv(1024).decode()
+            to_send_bids: list = []
+            for bid in self.bid.bids:
+                if bid['auction_id'] == int(auction_id):
+                    to_send_bids.append(bid)
+                else:
+                    continue
+            json_bids: json = json.dumps(to_send_bids)
+            sock.send(bytes(json_bids, 'utf-8'))
+            self.sec_menu(sock)
 
-                        sock.send(bytes("\nUser Registered Successfully!!", "utf-8"))
-                        continue
-                    elif option == '2':
-                        title, description, end_time = sock.recv(1024).decode().split('||')
-                        # temp_db to store recently created auction data
-                        auction_id: int = len(self.auction.auctions)+1
-                        temp_db: dict = {
-                            "id": str(auction_id),
-                            "title": title,
-                            "description": description,
-                            "end_time": end_time
-                        }
-                        self.auction.auctions.append(temp_db)  # appending temp_db to auctions
-                        self.auction.db_saving()  # saving auctions to database
+    def register(self, sock):
+        username, password, email, phone = sock.recv(1024).decode().split('||')
+        # temp_db to store recently registered user's data
+        temp_db: dict = {
+            "username": username,
+            "password": password,
+            "email": email,
+            "phone": phone
+        }
+        self.user.users.append(temp_db)  # appending temp_db to users
+        self.user.db_saving()  # saving users to database
 
-                        sock.send(bytes("\nAuction Created Successfully!!", "utf-8"))
-                        continue
-                    elif option == '3':
-                        pass
+        sock.send(bytes("\nUser Registered Successfully!!", "utf-8"))
+        self.menu(sock)
+
+    def login(self, sock):
+        option = sock.recv(1024).decode()
+        if option == '1':
+            email, password = sock.recv(1024).decode().split('||')
+            check: int = -1
+            for i in range(len(self.user.users)):
+                if self.user.users[i]['email'] == email:
+                    if self.user.users[i]['password'] == password:
+                        check = 0
+                        self.log_user_id = i
+                    else:
+                        check = 1
+                else:
+                    check = 2
+            if check == 0:
+                sock.send(bytes('\nWelcome User' + self.user.users[self.log_user_id]['username'], 'utf-8'))
+                self.sec_menu(sock)
+            elif check == 1:
+                sock.send(bytes('\nIncorrect Password!!', 'utf-8'))
+                self.login(sock)
+            elif check == 2:
+                sock.send(bytes('\nEmail Cannot Be Found!!', 'utf-8'))
+                self.login(sock)
+        elif option == '2':
+            phone, password = sock.recv(1024).decode().split('||')
+            check: int = -1
+            for i in range(len(self.user.users)):
+                if self.user.users[i]['phone'] == phone:
+                    if self.user.users[i]['password'] == password:
+                        check = 0
+                        self.log_user_id = i
+                    else:
+                        check = 1
+                else:
+                    check = 2
+            if check == 0:
+                sock.send(bytes('\nWelcome User ' + self.user.users[self.log_user_id]['username'], 'utf-8'))
+                self.sec_menu(sock)
+            elif check == 1:
+                sock.send(bytes('\nIncorrect Password!!', 'utf-8'))
+                self.login(sock)
+            elif check == 2:
+                sock.send(bytes('\nPhone Cannot Be Found!!', 'utf-8'))
+                self.login(sock)
+        elif option == '3':
+            self.menu(sock)
+
+    def create_auction(self, sock):
+        title, description, end_time, price = sock.recv(1024).decode().split('||')
+        # temp_db to store recently created auction data
+        auction_id: int = len(self.auction.auctions) + 1
+        temp_db: dict = {
+            "id": auction_id,
+            "title": title,
+            "description": description,
+            "end_time": end_time,
+            "price": int(price),
+            "highest_price": int(price)
+        }
+        self.auction.auctions.append(temp_db)  # appending temp_db to auctions
+        self.auction.db_saving()  # saving auctions to database
+
+        sock.send(bytes("\nAuction Created Successfully!!", "utf-8"))
+        self.sec_menu(sock)
 
 
 class User:
@@ -101,6 +201,7 @@ class User:
             print(err)
 
 
+
 class Auction:
     def __init__(self):
         # Auction loading from a file
@@ -118,7 +219,9 @@ class Auction:
                         'id': int(temp_auction[0]),
                         'title': temp_auction[1],
                         'description': temp_auction[2],
-                        'end_time': temp_auction[3][:-1],
+                        'end_time': temp_auction[3],
+                        "price": int(temp_auction[4]),
+                        "highest_price": int(temp_auction[5][:-1]),
                     }
                     self.auctions.append(temp_dict)
         except Exception as err:
@@ -129,8 +232,45 @@ class Auction:
         try:
             with open("Auctions.txt", "w") as file:
                 for i in range(len(self.auctions)):
-                    file.write(self.auctions[i]['id'] + '||' + self.auctions[i]['title'] + '||' + self.auctions[i][
-                        'description'] + '||' + self.auctions[i]['end_time'] + '\n')
+                    file.write(str(self.auctions[i]['id']) + '||' + self.auctions[i]['title'] + '||' + self.auctions[i][
+                        'description'] + '||' + self.auctions[i]['end_time'] + '||'
+                               + str(self.auctions[i]['price']) + '||' + str(self.auctions
+                                                                             [i]['highest_price']) + '\n')
+        except Exception as err:
+            print(err)
+
+
+class Bid:
+    def __init__(self):
+        # db of bids
+        self.bids: list = []
+        self.db_loading()
+
+    def db_loading(self):
+        # Load user data from a file
+        try:
+            with open("Bids.txt", "r") as file:
+                bids_data: list = file.readlines()
+                for bid in bids_data:
+                    temp_bid: list = bid.split('||')
+                    temp_dict: dict = {
+                        'auction_id': int(temp_bid[0]),
+                        'user': temp_bid[1],
+                        'bid_amount': int(temp_bid[2]),
+                        'bid_time': temp_bid[3][:-1]
+                    }
+                    self.bids.append(temp_dict)
+        except Exception as err:
+            print(err)
+
+    def db_saving(self):
+        # Save user data to a file
+        try:
+            with open("Bids.txt", "w") as file:
+                for i in range(len(self.bids)):
+                    file.write(str(self.bids[i]['auction_id']) + '||' + self.bids[i]['user'] + '||' + str(self.bids[i][
+                        'bid_amount']) + '||' + self.bids[i][
+                        'bid_time'] + '\n')
         except Exception as err:
             print(err)
 
